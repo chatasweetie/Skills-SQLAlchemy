06725b48b54b8073b27b767cf9c4a4db50d61081
Jessica <jessica.dene.earley@gmail.com>
2015-10-24 16:11:08 -0700

    did section 2.5

---
+++ model.py
@@ -0,0 +1,83 @@
+"""Models and database functions for Ratings project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# This is the connection to the SQLite database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+##############################################################################
+# Part 1: Compose ORM
+
+class Model(db.Model):
+
+    __tablename__ = "models"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.Integer, nullable=False)
+    brand_name = db.Column(db.Integer, nullable=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        """Provides helpful representation of a model object when printed"""
+
+        return "<Model: year= {} Brand Name={} Model={}>".format(self.year, 
+                                                     self.brand_name, self.name)
+
+
+class Brand(db.Model):
+
+    __tablename__ = "brands"
+    
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(50), db.ForeignKey('models.brand_name'), 
+                                                         nullable=False)
+    founded = db.Column(db.Integer)
+    headquarters = db.Column(db.String(50))
+    discontinued = db.Column(db.Integer)
+
+    #defines the relationship from brands to models
+    brand = db.relationship("Model", backref=db.backref("brands"))
+
+    def __repr__(self):
+        """Provides helpful representation of a brand object when printed"""
+
+        return "<Brand: name={} founded={}>".format(self.name, self.founded)
+
+# End Part 1
+##############################################################################
+# Helper functions
+
+
+def init_app():
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app
+    from flask import Flask
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print "Connected to DB."
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our SQLite database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auto.db'
+    app.config['SQLALCHEMY_ECHO'] = True
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app
+    from flask import Flask
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print "Connected to DB."
